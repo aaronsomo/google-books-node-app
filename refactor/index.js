3dde1a561184b354d0a151d0acb9d4b92011a523
Aaron Southammavong <aaroneight07@gmail.com>
2020-04-24 10:56:05 -0500

    added intro screen; no functionality yet

--- refactor/index.js
+++ refactor/index.js
@@ -2,4 +2,13 @@ const App = require('./app');
 
 const app = new App();
 
-app.init();
+// app.init();
+
+const introScreen = () => {
+  console.log('\n--- Your Personal Reading List App ---\n');
+  console.log('1. View your current Reading List');
+  console.log('2. Start a new search');
+  console.log('3. Exit\n');
+};
+
+introScreen();
