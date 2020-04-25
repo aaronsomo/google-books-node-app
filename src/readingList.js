573c5eae1591e8ec0d92d79460ecdffdf48c9420
Aaron Southammavong <aaroneight07@gmail.com>
2020-04-25 11:32:08 -0500

    added edge case in the event that lists are not arrays; hooked up class Book

--- src/readingList.js
+++ src/readingList.js
@@ -3,13 +3,20 @@ const Book = require('./book').Book;
 class ReadingList {
   constructor(data) {
     this.booklist = [];
-    data.forEach(({ volumeInfo }) => {
-      this.booklist.push({
-        title: volumeInfo.title,
-        authors: volumeInfo.authors || 'unknown',
-        publisher: volumeInfo.publisher || 'unknown',
+    if (Array.isArray(data)) {
+      data.forEach(({ volumeInfo }) => {
+        this.booklist.push(
+          new Book({
+            title: volumeInfo.title,
+            authors: volumeInfo.authors || 'unknown',
+            publisher: volumeInfo.publisher || 'unknown',
+          })
+        );
       });
-    });
+    } else {
+      this.books = [];
+      console.log('Error creating list...');
+    }
   }
 }
 
